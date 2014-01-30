01f600bed91ff261cf15b673bb8dfe265308eac9
vincent daubry <vdaubry@gmail.com>
2014-01-30 19:24:37 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhotoVisualizer::Application.config.secret_key_base = '32e0f30fb361d902d5561e55127288fdc3db525e1afc20d2a914c31fc566102708e06170b1a99a66f901e1e917a59ad358463ff114a12e4c52161af9f1cbadeb'

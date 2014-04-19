b0fb5454ed9525236e88b7efb787ff72e7385cee
Vincent Daubry <vdaubry@gmail.com>
2014-04-19 16:13:51 +0200

    ajout d'un bouton banish post

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,7 +4,6 @@ describe PostsController do
   render_views
 
   describe "DELETE destroy" do
-
     context "has next post" do
       it "redirects to next post" do
         stub_request(:delete, "http://localhost:3002/websites/123/posts/456.json")
@@ -26,6 +25,34 @@ describe PostsController do
                   :status => 200)
 
         delete 'destroy', :id => 456, :website_id => 123
+
+        response.should redirect_to root_path
+      end
+    end
+  end
+
+  describe "PUT banish" do
+    context "has next post" do
+      it "redirects to next post" do
+        stub_request(:put, "http://localhost:3002/websites/123/posts/456/banish.json")
+        .to_return(:headers => {"Content-Type" => 'application/json'},
+                  :body => '{"latest_post":"506144650ed4c08d84000001"}', 
+                  :status => 200)
+
+        put 'banish', :id => 456, :website_id => 123
+
+        response.should redirect_to website_post_images_path(123, "506144650ed4c08d84000001", :status => "TO_SORT_STATUS")
+      end
+    end
+
+    context "no more posts" do
+      it "redirects to root_path" do
+        stub_request(:put, "http://localhost:3002/websites/123/posts/456/banish.json")
+        .to_return(:headers => {"Content-Type" => 'application/json'},
+                  :body => '{"latest_post":null}', 
+                  :status => 200)
+
+        put 'banish', :id => 456, :website_id => 123
 
         response.should redirect_to root_path
       end
